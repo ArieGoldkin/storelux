976c416b3e9247740a18ee9c0dfc116dca9e928b
ariegold <ariegoldkin88@gmail.com>
2020-09-08 14:18:06 +0300

    categories selectors added

---
+++ src/components/categoriesComponents/categoriesSelectors.js
@@ -0,0 +1,7 @@
+export const getCategories = (state) => state.categories.items;
+
+export const getCategoriesLoading = (state) => state.categories.loading;
+
+export const getCategoriesError = (state) => state.categories.error;
+
+export const getCategoriesIsDone = (state) => state.categories.isDone;
