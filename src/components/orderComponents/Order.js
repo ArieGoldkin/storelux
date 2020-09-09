e9c87f281f060dfe7f54d232228d7ed520e55616
ariegold <ariegoldkin88@gmail.com>
2020-09-09 19:17:39 +0300

    spliting the order and create orderItem

--- src/components/orderComponents/Order.js
+++ src/components/orderComponents/Order.js
@@ -4,9 +4,10 @@ import { useParams } from "react-router-dom";
 
 import LoadingSpinner from "../common/UIElements/LoadingSpinner";
 import ErrorModal from "../common/UIElements/ErrorModal";
-import OrderItem from "./OrderItem";
+import OrderInfo from "./OrderInfo";
 import ShoppingCartSummary from "../shoppingCartComponents/ShoppingCartSummary";
 import * as cartSelectors from "../shoppingCartComponents/selectors/CartSelectors";
+import * as authSelectors from "../userComponents//selectors/AuthSelectors";
 import * as userSelectors from "../userComponents/selectors/UserSelectors";
 import * as orderSelecotrs from "./selectors/OrderSelectors";
 import * as actions from "./orderActions/OrderActions";
@@ -20,6 +21,7 @@ const Order = (props) => {
     orderSummary,
     orderSummaryLoading,
     orderLoading,
+    orderIsDone,
   } = props;
   const { pcartId } = useParams();
   const [totalPrice, setTotalPrice] = useState();
@@ -33,7 +35,7 @@ const Order = (props) => {
   // console.log(products);
 
   useEffect(() => {
-    if (orderSummaryLoading || orderLoading) {
+    if (!orderIsDone) {
       setIsLoading(true);
     } else {
       setIsLoading(false);
@@ -42,6 +44,7 @@ const Order = (props) => {
     setTotalPrice(orderSummary.totalPrice);
     setTotalSum(orderSummary.totalSum);
   }, [
+    orderIsDone,
     orderLoading,
     orderSummary.totalPrice,
     orderSummary.totalSum,
@@ -57,6 +60,13 @@ const Order = (props) => {
     setOrder(product);
   }, [product, setOrder]);
 
+  // useEffect(() => {
+  //   if (canRemove) {
+  //     debugger;
+  //     onSuccessDeletefromCart(token, userId, product);
+  //   }
+  // }, [canRemove, onSuccessDeletefromCart, product, token, userId]);
+
   return (
     <>
       <ErrorModal error={errorMessage} onClear={clearError} />
@@ -68,7 +78,7 @@ const Order = (props) => {
         )}
         {!isLoading && (
           <>
-            <OrderItem product={product} user={user} />
+            <OrderInfo product={product} user={user} />
             <ShoppingCartSummary
               item={product}
               totalPrice={totalPrice}
@@ -84,16 +94,22 @@ const Order = (props) => {
 
 const mapStateToProps = (state) => {
   return {
+    userId: authSelectors.getAuthUserId(state),
+    token: authSelectors.getAuthtoken(state),
     products: cartSelectors.getCartItems(state),
     user: userSelectors.getUserItem(state),
     orderSummaryLoading: orderSelecotrs.getOrderSummaryLoading(state),
     orderLoading: orderSelecotrs.getOrderLoading(state),
     orderSummary: orderSelecotrs.getOrderSummary(state),
+    canRemove: orderSelecotrs.getOrderCanRemove(state),
+    orderIsDone: orderSelecotrs.getOrderIsDone(state),
   };
 };
 const mapDisptachToProps = (dispatch) => {
   return {
     setOrder: (product) => dispatch(actions.setOrderRequest(product)),
+    // onSuccessDeletefromCart: (token, userId, product) =>
+    //   dispatch(actions.DeleteFromCartAfterOrderRequest(token, userId, product)),
   };
 };
 
