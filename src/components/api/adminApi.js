59d0b6d3f6a74d28de03634f101844c653160f5d
ariegold <ariegoldkin88@gmail.com>
2020-09-26 23:12:40 +0300

    get global data and update global rate api calls added

--- src/components/api/adminApi.js
+++ src/components/api/adminApi.js
@@ -1,5 +1,25 @@
 import axios from "axios";
 
+export const getData = ({ token }) => {
+  return axios.get(`/api/admin/globaldata`, {
+    headers: {
+      Authorization: "Bearer " + token,
+    },
+  });
+};
+
+export const updateRate = ({ vatRate, adminId, token }) => {
+  return axios.patch(
+    `/api/admin/updaterate`,
+    { vatRate, adminId },
+    {
+      headers: {
+        Authorization: "Bearer " + token,
+      },
+    }
+  );
+};
+
 export const deleteProducts = ({ products, adminId, token }) => {
   return axios.delete(`/api/admin/deleteitems`, {
     headers: {
