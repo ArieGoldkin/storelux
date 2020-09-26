fb40d9e4458899f8d9e104c21030a48b1f248cc7
ariegold <ariegoldkin88@gmail.com>
2020-09-26 23:11:29 +0300

    calculate total price functions added to util folder

---
+++ src/components/common/util/calcTotalPrice.js
@@ -0,0 +1,28 @@
+export const calcSummary = (cart, vatRate) => {
+  let newSum = 0;
+  let sum;
+  let calcVat;
+  let total;
+  let totalPrice = cart.map((item) => item.price * item.quantity);
+  totalPrice.map((item) => (newSum += item));
+  calcVat = (newSum * vatRate).toFixed(2);
+  total = (newSum * vatRate + newSum).toFixed(2);
+  sum = newSum.toFixed(2);
+  return { sum, calcVat, total };
+};
+
+export const calcProductOrderSummary = (items, vatRate) => {
+  let newSum = 0;
+  let sum;
+  let calcVat;
+  let total;
+  let totalPrice = items.map((item) => item.quantity * item.price);
+
+  for (let i = 0; i < totalPrice.length; i++) {
+    newSum += totalPrice[i];
+  }
+  calcVat = (newSum * vatRate).toFixed(2);
+  total = (newSum * vatRate + newSum).toFixed(2);
+  sum = newSum.toFixed(2);
+  return { sum, calcVat, total };
+};
