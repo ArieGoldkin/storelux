cd0f584b35828a0e4a34adf213b53bed335edd22
ariegold <ariegoldkin88@gmail.com>
2020-10-03 14:13:21 +0300

    created global user api variable to user api file

--- src/components/api/usersApi.js
+++ src/components/api/usersApi.js
@@ -1,10 +1,12 @@
 import axios from "axios";
 
+const UsersAPI = "/api/users";
+
 export const getUsers = () => {
-  return axios.get("/api/users");
+  return axios.get(UsersAPI);
 };
 export const createUser = ({ firstName, lastName, email, password }) => {
-  return axios.post("/api/users/signup", {
+  return axios.post(`${UsersAPI}/signup`, {
     firstName,
     lastName,
     email,
@@ -13,16 +15,29 @@ export const createUser = ({ firstName, lastName, email, password }) => {
 };
 
 export const loginUser = ({ email, password }) => {
-  return axios.post("/api/users/login", {
+  return axios.post(`${UsersAPI}/login`, {
     email,
     password,
   });
 };
 
 export const getUserData = ({ userId }) => {
-  return axios.get(`/api/users/${userId}`);
+  return axios.get(`${UsersAPI}/${userId}`);
 };
 
 export const updateUserData = ({ userId, formData }) => {
-  return axios.patch(`/api/users/${userId}`, formData);
+  return axios.patch(`${UsersAPI}/${userId}`, formData);
+};
+
+export const resetUserPassword = ({ email }) => {
+  return axios.post(`${UsersAPI}/resetPassword`, {
+    email,
+  });
+};
+
+export const updateUserPassword = ({ resetToken, password }) => {
+  return axios.post(`${UsersAPI}/updatePassword`, {
+    password,
+    resetToken,
+  });
 };
