379d17ac3cd9037981d034907b506a500d02e251
ariegold <ariegoldkin88@gmail.com>
2020-10-03 14:21:01 +0300

    reset password saga added to root saga

--- src/components/store/rootSaga.js
+++ src/components/store/rootSaga.js
@@ -1,22 +1,24 @@
 import { all } from "redux-saga/effects";
 import productsSagas from "../productComponents/productsSaga/productsSaga";
-import addTocartSagas from "../productComponents/productsSaga/addToCartSaga";
+import addToCartSagas from "../productComponents/productsSaga/addToCartSaga";
 import usersSagas from "../userComponents/userSagas/UserSaga";
 import authSagas from "../userComponents/userSagas/authSaga";
 import categoriesSagas from "../categoriesComponents/categoriesSaga";
 import cartSagas from "../shoppingCartComponents/shoppingCartSagas/ShoppingCartSaga";
 import orderSagas from "../orderComponents/orderSagas/OrderSagas";
 import adminSagas from "../adminComponents/adminSaga/adminSagas";
+import ResetPasswordSagas from "../userComponents/userSagas/ResetPasswordSaga";
 
 const combineSagas = [
   ...productsSagas,
-  ...addTocartSagas,
+  ...addToCartSagas,
   ...usersSagas,
   ...authSagas,
   ...categoriesSagas,
   ...cartSagas,
   ...orderSagas,
   ...adminSagas,
+  ...ResetPasswordSagas,
 ];
 
 export default function* rootSaga() {
