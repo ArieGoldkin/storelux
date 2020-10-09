ebfc0086002328eaa11d4a4c34a97a6befe95107
ariegold <ariegoldkin88@gmail.com>
2020-10-09 20:35:26 +0300

    all products and get products by name in admin page added

--- src/components/api/adminApi.js
+++ src/components/api/adminApi.js
@@ -1,16 +1,18 @@
 import axios from "axios";
 
+const adminAPI = "/api/admin";
+
 export const getData = ({ token }) => {
-  return axios.get(`/api/admin/globaldata`, {
+  return axios.get(`${adminAPI}/globaldata`, {
     headers: {
       Authorization: "Bearer " + token,
     },
   });
 };
 
-export const getAllOrders = ({ token, adminId, fromDate, toDate }) => {
+export const getOrdersByDate = ({ token, adminId, fromDate, toDate }) => {
   return axios.post(
-    `/api/admin/allorders`,
+    `${adminAPI}/ordersbydate`,
     { adminId, fromDate, toDate },
     {
       headers: {
@@ -20,21 +22,33 @@ export const getAllOrders = ({ token, adminId, fromDate, toDate }) => {
   );
 };
 
-// export const getAllOrders = ({ token, adminId }) => {
-//   return axios.post(
-//     `/api/admin/allorders`,
-//     { adminId },
-//     {
-//       headers: {
-//         Authorization: "Bearer " + token,
-//       },
-//     }
-//   );
-// };
+export const getAllProducts = ({ token, adminId }) => {
+  return axios.post(
+    `${adminAPI}/allproducts`,
+    { adminId },
+    {
+      headers: {
+        Authorization: "Bearer " + token,
+      },
+    }
+  );
+};
+
+export const getOrdersByUserName = ({ token, adminId, userName }) => {
+  return axios.post(
+    `${adminAPI}/ordersByUserName`,
+    { adminId, userName },
+    {
+      headers: {
+        Authorization: "Bearer " + token,
+      },
+    }
+  );
+};
 
 export const updateRate = ({ vatRate, adminId, token }) => {
   return axios.patch(
-    `/api/admin/updaterate`,
+    `${adminAPI}/updaterate`,
     { vatRate, adminId },
     {
       headers: {
@@ -45,10 +59,34 @@ export const updateRate = ({ vatRate, adminId, token }) => {
 };
 
 export const deleteProducts = ({ products, adminId, token }) => {
-  return axios.delete(`/api/admin/deleteitems`, {
+  return axios.delete(`${adminAPI}/deleteitems`, {
     headers: {
       Authorization: "Bearer " + token,
     },
     data: { products, adminId },
   });
+};
+
+export const addCategory = ({ adminId, token, name }) => {
+  return axios.post(
+    `${adminAPI}/addcategory`,
+    { adminId, name },
+    {
+      headers: {
+        Authorization: "Bearer " + token,
+      },
+    }
+  );
+};
+
+export const productStatusChange = ({ adminId, token, productId }) => {
+  return axios.patch(
+    `${adminAPI}/statuschange`,
+    { adminId, productId },
+    {
+      headers: {
+        Authorization: "Bearer " + token,
+      },
+    }
+  );
 };
