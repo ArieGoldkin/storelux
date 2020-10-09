d07d009a4c42253036a83fa8086fce78b35b88a0
ariegold <ariegoldkin88@gmail.com>
2020-10-09 20:39:34 +0300

    admin get orders by name added to reducer

--- src/components/adminComponents/adminReducer/adminAllOrdersReducer.js
+++ src/components/adminComponents/adminReducer/adminAllOrdersReducer.js
@@ -20,7 +20,7 @@ const getOrdersSuccess = (state, action) => {
     items: action.payload.orders,
     loading: false,
     error: null,
-    isDone:true,
+    isDone: true,
   });
 };
 
@@ -32,14 +32,37 @@ const getOrdersFailure = (state, action) => {
   });
 };
 
+const getOrdersByUserNameSuccess = (state, action) => {
+  return updateObject(state, {
+    items: action.payload.items,
+    loading: false,
+    error: null,
+    isDone: true,
+  });
+};
+
+const getOrdersByUserNameFailure = (state, action) => {
+  return updateObject(state, {
+    error: action.payload.error,
+    loading: false,
+    isDone: true,
+  });
+};
+
 export default function adminAllOrdersReducer(state = initialState, action) {
   switch (action.type) {
-    case Types.GET_ALL_ORDERS_REQUEST:
+    case Types.GET_ORDERS_BY_DATE_REQUEST:
+    case Types.GET_ORDERS_BY_USER_NAME_REQUEST:
       return getOrdersRequest(state, action);
-    case Types.GET_ALL_ORDERS_SUCCESS:
+    case Types.GET_ORDERS_BY_DATE_SUCCESS:
       return getOrdersSuccess(state, action);
+    case Types.GET_ORDERS_BY_USER_NAME_SUCCESS:
+      return getOrdersByUserNameSuccess(state, action);
     case Types.GET_GLOBAL_DATA_FAILURE:
+    case Types.GET_ORDERS_BY_DATE_FAILURE:
       return getOrdersFailure(state, action);
+    case Types.GET_ORDERS_BY_USER_NAME_FAILURE:
+      return getOrdersByUserNameFailure(state, action);
     default:
       return state;
   }
