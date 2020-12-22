72fbade830afd76d481198e4378f5243c1b3020a
ArieGoldkin <ariegoldkin88@gmail.com>
2020-12-22 20:28:55 +0200

    add redirect selector

--- src/store/selectors/products/updateProduct.js
+++ src/store/selectors/products/updateProduct.js
@@ -3,3 +3,6 @@ export const getUpdateProduct = (state) => state.updateProduct.item;
 export const getUpdateProductLoading = (state) => state.updateProduct.loading;
 
 export const getUpdateProductError = (state) => state.updateProduct.error;
+
+export const getUpdateProductRedirect = (state) =>
+  state.updateProduct.canRedirect;
